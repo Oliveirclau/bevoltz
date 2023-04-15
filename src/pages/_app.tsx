a49a72e40765029d07ec1783420003977ab9d2d0
David Augusto <davidribeiro086@gmail.com>
2023-04-15 01:31:12 -0300

    chore: eslint import helpers config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import GlobalStyles from '@/styles/global'
-import defaultTheme from '@/styles/theme'
 import type { AppProps } from 'next/app'
+
 import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from 'styles/global'
+import defaultTheme from 'styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
